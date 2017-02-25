5470a27b9755eb397d9593c33bd0ca55fc8fe042
Jonathan Funk <jonfunk88@hotmail.com>
2017-02-25 14:35:20 -0800

    'where' test made

--- __tests__/question.spec.js
+++ __tests__/question.spec.js
@@ -11,7 +11,6 @@ describe('question', () => {
       const output = test.subject(input);
       expect(output).toEqual('This is not a question!');
     });
-
     describe('.listen', () => {
       describe('when passed string that starts with "when"', () => {
         it('should return a string containing am or pm', () => {
@@ -43,18 +42,7 @@ describe('question', () => {
             });
           });
         });
-        describe('when the string asks for "who"', () => {
-          describe('when the string contains "who"', () => {
-            it('should return a string cotaining "is who"', () => {
-              const isWhoRegex = /.*is.+who.*/i;
-              const input = 'Who is this person?';
-              const output = test.subject(input);
-              const result = isWhoRegex.test(output);
-              expect(result).toBe(true);
-            });
-          });
-        });
-        describe('when the string contains "age"', () => {
+        describe('when the string does not contain "how old"', () => {
           describe('when the string contains "age" ', () => {
             it('should return a string containing a number', () => {
               const regexNumbers = /.*[0-9].*/;
@@ -65,15 +53,37 @@ describe('question', () => {
             });
           });
         });
-        describe('when the string contains "how old"', () => {
-          describe('when the string contains "how old" ', () => {
-            it('should return a string containing a number', () => {
-              const regexNumbers = /.*[0-9].*/;
-              const input = 'What age are you?';
-              const output = test.subject(input);
-              const result = regexNumbers.test(output);
-              expect(result).toBe(true);
-            });
+      });
+      describe('when the string does not contain "age"', () => {
+        describe('when the string contains "how old" ', () => {
+          it('should return a string containing a number', () => {
+            const regexNumbers = /.*[0-9].*/;
+            const input = 'What age are you?';
+            const output = test.subject(input);
+            const result = regexNumbers.test(output);
+            expect(result).toBe(true);
+          });
+        });
+      });
+      describe('when the string contains for "who"', () => {
+        describe('when the string contains "who"', () => {
+          it('should return a string containing "is who"', () => {
+            const isWhoRegex = /.*is.+who.*/i;
+            const input = 'Who is this person?';
+            const output = test.subject(input);
+            const result = isWhoRegex.test(output);
+            expect(result).toBe(true);
+          });
+        });
+      });
+      describe('when the string contains "where"', () => {
+        describe('when the string only contains "where"', () => {
+          it('should return a string containing "house"', () => {
+            const whereRegex = /.*house.*/i;
+            const input = 'Where do you want to go?';
+            const output = test.subject(input);
+            const result = whereRegex.test(output);
+            expect(result).toBe(true);
           });
         });
       });
