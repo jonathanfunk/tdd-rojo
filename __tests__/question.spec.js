a319961a82a2106402cbc5a3e1a3e5c209d06ad9
Jonathan Funk <jonfunk88@hotmail.com>
2017-02-25 14:05:25 -0800

    cleaned up code & modified 'age' test

--- __tests__/question.spec.js
+++ __tests__/question.spec.js
@@ -3,28 +3,15 @@ describe('question', () => {
 
   describe('when valid values are passed', () => {
     beforeEach(() => {
-      /*
-      jest.mock('../lib/rojo-thinks', () => ({
-        rojoThinks: jest.fn(() => '4:30pm, amigo!'),
-      }));
-      */
-
       test = {};
       test.subject = require('../lib/question');
-      //test.rojoThinks = require('../lib/rojo-thinks');
+    });
+    it('should return a string "This is not a question!"', () => {
+      const input = 'Who is this person';
+      const output = test.subject(input);
+      expect(output).toEqual('This is not a question!');
     });
 
-    it('should return an string answer', () => {
-      const testString = '4:30pm, amigo!';
-      expect(test.subject(testString)).toEqual("This is not a question!");
-    });
-
-    /*
-    it('should pass correct params to rojoThinks', () => {
-      test.subject('What time can I start drinking beer on Friday?');
-      expect(test.rojoThinks.rojoThinks).toHaveBeenCalledWith('What time can I start drinking beer on Friday?');
-    });
-    */
     describe('.listen', () => {
       describe('when passed string that starts with "when"', () => {
         it('should return a string containing am or pm', () => {
@@ -67,20 +54,22 @@ describe('question', () => {
             });
           });
         });
-        describe('when the string is a question', () => {
-          describe('when the string does not contain a "?" at the end', () => {
-            it('should return a string "This is not a question!"', () => {
-              const input = 'Who is this person';
-              const output = test.subject(input);
-              expect(output).toEqual('This is not a question!');
-            });
-          });
-        });
         describe('when the string contains "age"', () => {
-          describe('when the string contains "age" or "how old" ', () => {
+          describe('when the string contains "age" ', () => {
             it('should return a string containing a number', () => {
               const regexNumbers = /.*[0-9].*/;
               const input = 'How old are you?';
+              const output = test.subject(input);
+              const result = regexNumbers.test(output);
+              expect(result).toBe(true);
+            });
+          });
+        });
+        describe('when the string contains "how old"', () => {
+          describe('when the string contains "how old" ', () => {
+            it('should return a string containing a number', () => {
+              const regexNumbers = /.*[0-9].*/;
+              const input = 'What age are you?';
               const output = test.subject(input);
               const result = regexNumbers.test(output);
               expect(result).toBe(true);
