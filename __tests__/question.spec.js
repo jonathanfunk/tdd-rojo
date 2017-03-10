7cff1f8e99e67a92be85093adeb55d90cf3da3f2
Jonathan Funk <jonfunk88@hotmail.com>
2017-03-10 14:58:57 -0800

    shortened describes

--- __tests__/question.spec.js
+++ __tests__/question.spec.js
@@ -13,39 +13,30 @@ describe('question', () => {
     describe('listening', () => {
       describe('when passed string that starts with "when"', () => {
         it('should return a string containing am or pm', () => {
-          const output = test.subject('When whould we do this?');
-          expect(/.+am|pm.*/i.test(output)).toBe(true);
+
         });
       });
       describe('when passed a string that starts with "what"', () => {
         describe('when the string contains "time"', () => {
           it('should return a string containing am or pm', () => {
-            const timeRegex = /.+am|pm.*/i;
-            const input = 'What time and what day?';
-            const output = test.subject(input);
-            const result = timeRegex.test(output);
-            expect(result).toBe(true);
+            const output = test.subject('What time and what day?');
+            expect(/.+am|pm.*/i.test(output)).toBe(true);
           });
         });
         describe('when the string does not contain "time"', () => {
           describe('when the string also contains "day"', () => {
             it('should return a string cotaining a day of the week', () => {
               const dayRegex = /.*monday|tuesday|wednesday|thursday|friday|saturday|sunday.*/i;
-              const input = 'What time and what day?';
-              const output = test.subject(input);
-              const result = dayRegex.test(output);
-              expect(result).toBe(true);
+              const output = test.subject('What time and what day?');
+              expect(dayRegex.test(output)).toBe(true);
             });
           });
         });
         describe('when the string does not contain "how old"', () => {
           describe('when the string contains "age" ', () => {
             it('should return a string containing a number', () => {
-              const regexNumbers = /.*[0-9].*/;
-              const input = 'How old are you?';
-              const output = test.subject(input);
-              const result = regexNumbers.test(output);
-              expect(result).toBe(true);
+              const output = test.subject('How old are you?');
+              expect(/.*[0-9].*/.test(output)).toBe(true);
             });
           });
         });
@@ -53,62 +44,44 @@ describe('question', () => {
       describe('when the string does not contain "age"', () => {
         describe('when the string contains "how old" ', () => {
           it('should return a string containing a number', () => {
-            const regexNumbers = /.*[0-9].*/;
-            const input = 'What age are you?';
-            const output = test.subject(input);
-            const result = regexNumbers.test(output);
-            expect(result).toBe(true);
+            const output = test.subject('What age are you?');
+            expect(/.*[0-9].*/.test(output)).toBe(true);
           });
         });
       });
       describe('when the string contains for "who"', () => {
         describe('when the string only contains "who"', () => {
           it('should return a string containing "is who"', () => {
-            const isWhoRegex = /.*is.+who.*/i;
-            const input = 'Who is this person?';
-            const output = test.subject(input);
-            const result = isWhoRegex.test(output);
-            expect(result).toBe(true);
+            const output = test.subject('Who is this person?');
+            expect(/.*is.+who.*/i.test(output)).toBe(true);
           });
         });
         describe('when the string also contains "favorite"', () => {
           it('should return a string containing "my favorite"', () => {
-            const whoFavoriteRegex = /.*my.+favorite.*/i;
-            const input = 'Who is your favorite movie star?';
-            const output = test.subject(input);
-            const result = whoFavoriteRegex.test(output);
-            expect(result).toBe(true);
+            const output = test.subject('Who is your favorite movie star?');
+            expect(/.*my.+favorite.*/i.test(output)).toBe(true);
           });
         });
       });
       describe('when the string contains "where"', () => {
         describe('when the string only contains "where"', () => {
           it('should return a string containing "house"', () => {
-            const whereRegex = /.*house.*/i;
-            const input = 'Where do you want to go?';
-            const output = test.subject(input);
-            const result = whereRegex.test(output);
-            expect(result).toBe(true);
+            const output = test.subject('Where do you want to go?');
+            expect(/.*house.*/i.test(output)).toBe(true);
           });
         });
         describe('when the string also contains "favorite"', () => {
           it('should return a string containing "my favorite place"', () => {
-            const whereFavoriteRegex = /.*my.+favorite.+place*/i;
-            const input = 'Where is your favorite place to study?';
-            const output = test.subject(input);
-            const result = whereFavoriteRegex.test(output);
-            expect(result).toBe(true);
+            const output = test.subject('Where is your favorite place to study?');
+            expect(/.*my.+favorite.+place*/i.test(output)).toBe(true);
           });
         });
       });
       describe('when the string is a yes or no question', () => {
         describe('when the string contains "is", "do", "does", "are" & "will"', () => {
           it('should return a string containing "yes" or "no"', () => {
-            const yesNoRegex = /.*yes|no.*/i;
-            const input = 'Do you want to build a snowman?';
-            const output = test.subject(input);
-            const result = yesNoRegex.test(output);
-            expect(result).toBe(true);
+            const output = test.subject('Do you want to build a snowman?');
+            expect(/.*yes|no.*/i.test(output)).toBe(true);
           });
         });
       });
